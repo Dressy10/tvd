0f604b04d5acef16f1ee6c203bcdc473c9033c78
Samuel Dressman <111008110+Dressy10@users.noreply.github.com>
2026-04-15 11:05:01 +0100

    Refactor imports in App.tsx

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,8 +1,7 @@
 import { useEffect, useRef, useState } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { 
-  ArrowRight,
+import {
   Menu,
   X,
   Palette,
