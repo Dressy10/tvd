7e7c1ec4a124ccbe0e9fc3538958c8fbdff6a051
Dressy10 <samueldressman20@gmail.com>
2026-04-20 13:10:36 +0100

    editing form

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -521,7 +521,7 @@ function App() {
                 >
                   <input type="hidden" name="access_key" value="cc48d5d6-73ed-46d9-80a8-d53f52643aeb" />
                   <input type="hidden" name="redirect" value="https://tvdynasty.org" />
-                  <input type="hidden" name="subject" value="New Booking Request - tvD Website!" />
+                  <input type="hidden" name="subject" value="New Booking Request" />
                   <input type="hidden" name="from_name" value="tvD Notifications" />
 
                   <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
